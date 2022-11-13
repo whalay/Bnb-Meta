f9d408c2696717c415b3ddbce1cd49dd79495733
Whalay <soliuy5@gmail.com>
2022-11-13 12:29:19 +0100

    restyled the placee to meet component

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -46,12 +46,16 @@ const Navbar = (props) => {
       )}
 
       {toggle && (
-        <div className="bg-[#A02279] absolute h-96 w-1/2 top-20 left-0 rounded-xl p-10">
-          <ul className="md:flex justify-between space-y-5 text-white text-2xl">
-            <li>Home</li>
-            <li>Place to stay</li>
-            <li>NFTs</li>
-            <li>Community</li>
+        <div className="bg-[#A02279] absolute h-[30rem] w-[80%] top-20 left-0 rounded-r-xl p-10">
+          <ul className="md:flex justify-between space-y-5 text-white text-xl">
+          <li>
+          <Link to="/home">Home</Link>
+        </li>
+        <li>
+          <Link to="/nft">Place to stay</Link>
+        </li>
+        <li>NFTs</li>
+        <li>Community</li>
           </ul>
 
           <button className="p-4 mt-10 bg-white text-black rounded-[10px] ">
