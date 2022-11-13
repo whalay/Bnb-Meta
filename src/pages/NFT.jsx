e0c15420c88dc707e1b607aac992a552606deaea
Whalay <soliuy5@gmail.com>
2022-11-13 12:23:59 +0100

    restyled the placee to meet component

--- src/pages/NFT.jsx
+++ src/pages/NFT.jsx
@@ -6,10 +6,11 @@ import  {Nft} from "../assets/Data";
 
 const NFT = () => {
   return (
-    <div className="h-screen">
-      {/* <Navbar /> */}
-      <div>
-        <ul className="flex">
+    <div className="">
+      <Navbar />
+      <div className="p-10">
+       <div className="flex justify-between items-center p-5 ">
+       <ul className="flex justify-between gap-20">
           <li>Restaurant</li>
           <li>Cottage</li>
           <li>Castle</li>
@@ -19,6 +20,8 @@ const NFT = () => {
           <li>Off-grid</li>
           <li>Farm</li>
         </ul>
+        <button className="p-2 px-4 border-2">Location</button>
+       </div>
         <div>
           <div className=" grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 place-items-center">
             {Nft.map((nft, index) => (
@@ -37,7 +40,7 @@ const NFT = () => {
           </div>
         </div>
       </div>
-      {/* <Footer /> */}
+      <Footer />
     </div>
   );
 };
